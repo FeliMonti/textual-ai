29efe76c2e4d93c33f5710c556a8a941b8879182
Emil <emil@pop-os.localdomain>
2022-02-18 12:15:24 +0100

    First upload

---
+++ Selectors.py
@@ -0,0 +1,7 @@
+
+
+class SelectBy():
+    VALUE = 'value'
+    INDEX = 'index'
+    VISIBLE_TEXT = 'visible_text'
+
---
+++ logins.py
@@ -0,0 +1,10 @@
+
+
+class Textual_AB():
+    NAME = 'textual-ab'
+    USER = 'regrtestaccount'
+    PW = '4wMo@&GO@J6'
+    MASTER_LANGUAGE = 'en_US'
+    
+    def __str__(self):
+        return f'Customer: {self.NAME}, account: {self.USER}'
